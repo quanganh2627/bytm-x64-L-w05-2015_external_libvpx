6a0275cd22297f2094b4b000a6ce7a8159085e8b
wfeng6 <wei.feng@intel.com>
2014-01-03 18:54:12 +0800

    Fix the VP8/VP9 CTS bug on CTP
    BZ: 161975
    
    Use the platform specific static library for libvpx.a. Previously,
    we use the libvpx.a generated on baytrail for all platforms. This causes
    problems on the CTP.
    
    Change-Id: I0fc52381aa964460a47b8160743e63844beda872
    Category: aosp improvement
    Domain: Video.Media-local playback
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: wfeng6 <wei.feng@intel.com>

--- x86/libvpx.mk
+++ x86/libvpx.mk
@@ -1,7 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_LIBS := lib/libvpx.a
+ifeq ($(TARGET_BOARD_PLATFORM),clovertrail)
+LOCAL_PREBUILT_LIBS := libs/clovertrail/libvpx.a
+else
+LOCAL_PREBUILT_LIBS := libs/common/libvpx.a
+endif
 
 LOCAL_MODULE_TAGS := optional
 
